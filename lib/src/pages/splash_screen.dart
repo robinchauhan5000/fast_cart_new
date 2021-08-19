85698bfd3a0f8d3650baf02f9e1cef7818a4ec6f
robinchauhan5000 <robinchauhan5000@gmail.com>
2021-08-19 12:48:38 +0530

    initial commit

---
+++ lib/src/pages/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:markets/utills/SizeConfig.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+
+import '../controllers/splash_screen_controller.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SplashScreenState();
+  }
+}
+
+class SplashScreenState extends StateMVC<SplashScreen> {
+  SplashScreenController _con;
+
+  SplashScreenState() : super(SplashScreenController()) {
+    _con = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() {
+    _con.progress.addListener(() {
+      double progress = 0;
+      _con.progress.value.values.forEach((_progress) {
+        progress += _progress;
+      });
+      if (progress == 100) {
+        try {
+          Navigator.of(context).pushReplacementNamed('/login', arguments: 2);
+        } catch (e) {}
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _con.scaffoldKey,
+      body: Container(
+        height: SizeConfig.screenHeight,
+        width: SizeConfig.screenWidth,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/splash.png'),
+          ),
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        margin: EdgeInsets.only(bottom: 30),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: CircularProgressIndicator(
+            valueColor:
+                AlwaysStoppedAnimation<Color>(Theme.of(context).hintColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
